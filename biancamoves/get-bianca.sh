2c3bb3475758dc299b6b9524862b8e6ba3820057
Netzach <r.jonas.soderberg@gmail.com>
2018-05-31 16:15:11 +0200

    Again?

--- biancamoves/get-bianca.sh
+++ biancamoves/get-bianca.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 #get-bianca.sh
-FILE=${*:-*} 
-sftp -r jonass-sens2016001@bianca-sftp.uppmax.uu.se<<EOF
-cd jonass-sens2016001/
+usage="$(basename "$0") [-h] [-u USERNAME on bianca] -p PROJECT_NAME <files_to_transfer>"
+while getopts ":hu:p:" option
+do
+    case $option in
+        h) echo "$usage"
+            exit 0
+            ;;
+        u) USER="$OPTARG"
+            ;;
+        p) PRO="$OPTARG"
+            ;;
+        :) printf "missing argument for -%s\n" "$OPTARG" >&2
+            echo "$usage"
+            exit 1
+            ;;
+        \?) printf "illegal option -%s\n" "$OPTARG" >&2
+            echo "$usage"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+me=${USER:-$(whoami)}
+proj=${PRO:-sens2016001}
+FILE=${*:-*}
+sftp -r $me-$proj@bianca-sftp.uppmax.uu.se<<EOF
+cd $me-$proj/
 get $FILE
 quit
 EOF
--- biancamoves/wharfput.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cp -av ${*:-*} /proj/sens2016001/nobackup/wharf/jonass/jonass-sens2016001/
