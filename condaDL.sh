8e09bc0b8e7c84ea6f254e40312c23147c7ddbaa
Netzach <r.jonas.soderberg@gmail.com>
2018-10-18 23:12:46 +0200

    Added SSL repo to script

--- condaDL.sh
+++ condaDL.sh
@@ -1,4 +1,5 @@
 cd /sw/apps/mc3/latest/rackham/repo/noarch
+wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://repo.anaconda.com/pkgs/main/linux-64
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://repo.continuum.io/pkgs/main/linux-64/
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://conda.anaconda.org/bioconda/linux-64/
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://conda.anaconda.org/conda-forge/linux-64/
@@ -11,6 +12,7 @@ wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://repo.continuum.io/pkgs/free/noarch/
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://repo.continuum.io/pkgs/r/noarch/
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://repo.continuum.io/pkgs/pro/noarch/
+wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://conda.anaconda.org/scilifelab-lts/linux-64
 mv repodata.json ../repodata.json.bak
 mv .index.json ../.index.json.bak
 module load mc3/latest
