9264c2020ebfd3125087bba1a2ec09573a58e1d2
Netzach <r.jonas.soderberg@gmail.com>
2018-11-20 15:34:30 +0100

    Now with write protection for the repo

--- condaDL.sh
+++ condaDL.sh
@@ -1,3 +1,4 @@
+chmod u+w /sw/apps/mc3/latest/rackham/repo/noarch
 cd /sw/apps/mc3/latest/rackham/repo/noarch
 wget -N -c -r -np -nH --cut-dirs=2 -R "index.html*" -R "repodata.json*" https://conda.anaconda.org/scilifelab-lts/linux-64/
 wget -N -c -r -np -nH --cut-dirs=3 -R "index.html*" -R "repodata.json*" https://repo.anaconda.com/pkgs/main/linux-64/
@@ -21,6 +22,7 @@ wget -N -c -r -np -nH --cut-dirs=4 -R "index.html*" -R "repodata.json*" https://
 wget -N -c -r -np -nH --cut-dirs=4 -R "index.html*" -R "repodata.json*" https://conda.anaconda.org/qiime2/label/r2018.11/linux-64/
 cp -av repodata.json ../repodata.json.bak
 cp -av .index.json ../.index.json.bak
+chmod -w /sw/apps/mc3/latest/rackham/repo/noarch
 module load mc3/latest
 conda update conda -y
 conda-index --channel-name CONDA_UPPMAX
