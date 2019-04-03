157bec19162ded2c0cd434a482e42be84eb6a9dc
Douglas G. Scofield <douglasgscofield@gmail.com>
2019-04-03 10:26:20 +0200

    Update BUSCO-update-v2-lineage-sets.sh

--- data_other/BUSCO/v2_lineage_sets/BUSCO-update-v2-lineage-sets.sh
+++ data_other/BUSCO/v2_lineage_sets/BUSCO-update-v2-lineage-sets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BLS_BASE=/sw/apps/bioinfo/BUSCO/v2_lineage_sets
+BLS_BASE=/sw/bioinfo/BUSCO/v2_lineage_sets
 WGET_OPTIONS="--quiet --timestamping"
 echo "Updating lineage sets in $BLS_BASE"
 cd $BLS_BASE
